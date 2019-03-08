ce9ff20464b11cf3d44b2fa34aa003dfb6984842
Sanket Ghodechor <sanket.ghodechor@gmail.com>
2019-03-08 14:32:26 +0530

    variable arguments list function

--- firstArguments.php
+++ firstArguments.php
@@ -2,14 +2,7 @@
 echo 'Parrameters passed to function as follows: ';
 echo "\n";
 variableArguments( 1,2,44,4 );
-
 function variableArguments( ...$var ) {
-
         print_r( $var );
-
-
 }
-
-
 ?>
-
